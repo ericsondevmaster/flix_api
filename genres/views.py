5f2b0784d12b3202123cbcf18ae9800365b21a6f
ericsondevmaster <ericson.devmaster@gmail.com>
2023-10-11 21:23:08 -0300

    Creating genre detail

--- genres/views.py
+++ genres/views.py
@@ -1,10 +1,11 @@
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
 @csrf_exempt
-def genre_view(request):
+def genre_create_list_view(request):
     if request.method == 'GET':
         genres = Genre.objects.all()
         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
@@ -14,3 +15,10 @@ def genre_view(request):
         new_genre = Genre(name=data['name'])
         new_genre.save()
         return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201,)
+
+
+@csrf_exempt
+def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+    data = {'id': genre.id, 'name': genre.name}
+    return JsonResponse(data)
