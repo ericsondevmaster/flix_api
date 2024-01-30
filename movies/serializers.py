b9f9b9d5e7f90f1fcf149340344e440f6afea46e
ericsondevmaster <ericson.devmaster@gmail.com>
2024-01-29 22:11:11 -0300

    Serializer Method Field

--- movies/serializers.py
+++ movies/serializers.py
@@ -3,17 +3,33 @@ from movies.models import Movie
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    rate = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
 
+    def get_rate(self, obj):
+        reviews = obj.reviews.all()
+        
+        if reviews:
+            sum_reviews = 0
+
+            for review in reviews:
+                sum_reviews += review.stars
+
+            reviews_count = reviews.count()
+
+            return round(sum_reviews / reviews_count, 1)
+
+        return None
+
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1900.')
         return value
     
     def validate_resume(self, value):
-        if len(value) > 200:
-            raise serializers.ValidationError('Resumo não deve ser maior do que 200 caracteres.')
+        if len(value) > 500:
+            raise serializers.ValidationError('Resumo não deve ser maior do que 500 caracteres.')
         return value
