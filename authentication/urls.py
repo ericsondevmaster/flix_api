8166efe27c98a02f93816c80f11c874cbd1dc0de
ericsondevmaster <ericson.devmaster@gmail.com>
2024-02-05 21:18:06 -0300

    Correction - Verify and Refresh Token

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,6 +6,6 @@ from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('authentication/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path('authentication/token/verify', TokenVerifyView.as_view(), name='token_verify'),
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('authentication/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
