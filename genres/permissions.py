87d3c64ee61c5db4359e51aebf2a738c64f26402
ericsondevmaster <ericson.devmaster@gmail.com>
2024-02-21 21:57:02 -0300

    Configuring permissions on endpoints

--- genres/permissions.py
+++ genres/permissions.py
@@ -9,5 +9,11 @@ class GenrePermissionClass(permissions.BasePermission):
         
         if request.method == 'POST':
             return request.user.has_perm('genres.add_genre')
+        
+        if request.method in ['PATCH', 'PUT']:
+            return request.user.has_perm('genres.change_genre')
+        
+        if request.method == 'DELETE':
+            return request.user.has_perm('genres.delete_genre')
 
         return False
