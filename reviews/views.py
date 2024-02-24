2bbe7e939336c8cd15989ad357ebc62d73ce1d85
ericsondevmaster <ericson.devmaster@gmail.com>
2024-02-24 16:44:34 -0300

    Implementing global permissions class

--- reviews/views.py
+++ reviews/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
+from app.permissions import GlobalDefaultPermission
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
